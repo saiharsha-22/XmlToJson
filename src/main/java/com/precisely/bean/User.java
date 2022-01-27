e1215dff64ec7bb6a3e377a8a4a1312dfb8f44a2
saiharsha-22 <harshavardhangood@gmail.com>
2022-01-27 15:17:48 +0530

    XmltoJson

---
+++ src/main/java/com/precisely/bean/User.java
@@ -0,0 +1,39 @@
+package com.precisely.bean;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "user")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class User {
+
+	private String name;
+	private int age;
+    
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
