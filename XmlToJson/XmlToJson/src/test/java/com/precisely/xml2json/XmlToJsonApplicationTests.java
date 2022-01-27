0cdc157cb803620776d8984e754fa0d880e8599d
samanway923 <samanway923@gmail.com>
2022-01-27 17:37:35 +0530

    First prototype

---
+++ XmlToJson/XmlToJson/src/test/java/com/precisely/xml2json/XmlToJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.precisely.xml2json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlToJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
